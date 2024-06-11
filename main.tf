430c7121cefaca19b0a1c04fa1c00eed0f9b50e8
Håkon Eriksen Drange <hakon.drange@soprasteria.com>
2024-06-11 11:10:31 +0200

    Rotation resources.

--- main.tf
+++ main.tf
@@ -115,23 +115,12 @@ resource "awscc_ssmcontacts_rotation" "business_hours" {
       {
         day_of_week   = "MON"
         hand_off_time = "08:30"
-      },
-      {
-        day_of_week   = "TUE"
-        hand_off_time = "08:30"
       }
     ]
 
     shift_coverages = [
       {
         day_of_week = "MON"
-        coverage_times = [{
-          start_time = "09:00"
-          end_time   = "16:00"
-        }]
-      },
-      {
-        day_of_week = "TUE"
         coverage_times = [{
           start_time = "09:00"
           end_time   = "16:00"
