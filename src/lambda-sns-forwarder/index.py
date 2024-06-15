b029bac3bc83dbb221a5d99e0e46c0a051e5df5d
Håkon Eriksen Drange <hakon.drange@soprasteria.com>
2024-06-15 11:10:36 +0200

    Lambda forwarder logic.

--- src/lambda-sns-forwarder/index.py
+++ src/lambda-sns-forwarder/index.py
@@ -6,6 +6,16 @@ logger = logging.getLogger()
 logger.setLevel("INFO")
 
 # Python boto3 logic to process SNS messages from topic defined in environment variable subscribed_sns_topic_arn and forwards the exact same content to SNS topic defined in environment variable target_sns_topic_arn
-
 def lambda_handler(event, context):
     logger.info(json.dumps(event))
+    sns = boto3.client('sns')
+    target_sns_topic_arn = os.environ['target_sns_topic_arn']
+    subscribed_sns_topic_arn = os.environ['subscribed_sns_topic_arn']
+    for record in event['Records']:
+        payload = json.dumps(record)
+        logger.info(f"Publishing message to SNS topic {target_sns_topic_arn}: {payload}")
+        sns.publish(TopicArn=target_sns_topic_arn, Message=payload)
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
