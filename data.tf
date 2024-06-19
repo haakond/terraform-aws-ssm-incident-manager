aabcf5080771a53733ea539023d9644cc15d6cac
Håkon Eriksen Drange <hakon.drange@soprasteria.com>
2024-06-19 22:20:49 +0200

    SSM runbook.

--- data.tf
+++ data.tf
@@ -12,7 +12,9 @@ data "aws_iam_policy_document" "policy_for_service_role_for_ssm_incident_manager
     resources = [
       "arn:aws:ssm:*:${local.aws_account_id}:automation-definition/AWSIncidents-CriticalIncidentRunbookTemplate:*",
       "arn:aws:ssm:*:${local.aws_account_id}:document/AWSIncidents-CriticalIncidentRunbookTemplate:*",
-      "arn:aws:ssm:*::automation-definition/AWSIncidents-CriticalIncidentRunbookTemplate:*"
+      "arn:aws:ssm:*::automation-definition/AWSIncidents-CriticalIncidentRunbookTemplate:*",
+      "arn:aws:ssm:eu-central-1:594179624893:automation-definition/${aws_ssm_document.critical_incident_runbook.name}:*",
+      "arn:aws:ssm:*:${local.aws_account_id}:document/${aws_ssm_document.critical_incident_runbook.name}:*",
     ]
   }
 }
