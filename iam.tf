af8fcd4526789bc8a37f5bbe78db85b0df07ee50
Håkon Eriksen Drange <hakon.drange@soprasteria.com>
2024-06-19 21:32:24 +0200

    SSM IAM.

--- iam.tf
+++ iam.tf
@@ -1,7 +1,6 @@
 resource "aws_iam_role" "service_role_for_ssm_incident_manager" {
-  name                = "aws-ssm-incident-manager-service-role"
-  assume_role_policy  = data.aws_iam_policy_document.assume_role_policy_for_service_role_for_ssm_incident_manager.json
-  managed_policy_arns = ["arn:aws:iam::aws:policy/aws-service-role/AWSIncidentManagerServiceRolePolicy"]
+  name               = "aws-ssm-incident-manager-service-role"
+  assume_role_policy = data.aws_iam_policy_document.assume_role_policy_for_service_role_for_ssm_incident_manager.json
 }
 
 resource "aws_iam_policy" "policy_for_service_role_for_ssm_incident_manager" {
